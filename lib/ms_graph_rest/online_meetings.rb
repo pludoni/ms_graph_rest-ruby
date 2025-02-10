0a0eed2b54e4293b15edfb127233e16cd7a6af24
Stefan Wienert <info@stefanwienert.de>
2025-02-10 15:24:32 +0000

    Feat: allow any online meeting parameter

--- lib/ms_graph_rest/online_meetings.rb
+++ lib/ms_graph_rest/online_meetings.rb
@@ -7,7 +7,7 @@ module MsGraphRest
     end
     Response.example('value' => [], "@odata.context" => "", "@odata.nextLink" => "")
 
-    def create(subject:, start_date_time:, end_date_time:, accept_language: "en", participants: nil, user_id: nil)
+    def create(subject:, start_date_time:, end_date_time:, accept_language: "en", participants: nil, user_id: nil, **args)
       start_date_time = start_date_time.iso8601 if start_date_time.respond_to?(:iso8601)
       end_date_time = end_date_time.iso8601 if end_date_time.respond_to?(:iso8601)
 
@@ -16,7 +16,7 @@ module MsGraphRest
         endDateTime: end_date_time,
         subject: subject,
         participants: participants
-      }
+      }.merge(args)
       path = user_id ? "users/#{CGI.escape(user_id)}/onlineMeetings" : "me/onlineMeetings"
       headers = {
         "Accept-Language" => accept_language
@@ -26,7 +26,7 @@ module MsGraphRest
 
     # https://docs.microsoft.com/en-us/graph/api/onlinemeeting-update
     def update(
-      id:, subject: nil, start_date_time: nil, end_date_time: nil, participants: nil, user_id: nil, accept_language: nil
+      id:, subject: nil, start_date_time: nil, end_date_time: nil, participants: nil, user_id: nil, accept_language: nil, **args
     )
       start_date_time = start_date_time.iso8601 if start_date_time.respond_to?(:iso8601)
       end_date_time = end_date_time.iso8601 if end_date_time.respond_to?(:iso8601)
@@ -36,7 +36,7 @@ module MsGraphRest
         endDateTime: end_date_time,
         subject: subject,
         participants: participants
-      }
+      }.merge(args)
       path = user_id ? "users/#{user_id}/onlineMeetings/#{id}" : "me/onlineMeetings/#{id}"
       headers = {
         "Accept-Language" => accept_language
